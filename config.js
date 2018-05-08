886cb6890270af2b64a5e0b44c8b435ba788198a
Hiram Cruz <hiram0101@gmail.com>
2018-05-08 11:59:55 -0400

    added passport-jwt support

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
-'use strict';
+"use strict";
 
 exports.PORT = process.env.PORT || 8080;
 
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
+exports.MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/noteful";
 
-exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
+exports.TEST_MONGODB_URI =
+  process.env.TEST_MONGODB_URI || "mongodb://localhost/noteful-test";
+
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
